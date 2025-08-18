c73a17cd54e286324980a2e5b173c6fead7aec7a
Chinoox96 <joaquin.ortigoza.96@gmail.com>
2025-08-17 23:02:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SW cache-first con precache básico y runtime cache
-const CACHE = 'f5-cache-v4';
+const CACHE = 'f5-cache-v5';
 const PRECACHE = ['./']; // raíz
 
 self.addEventListener('install', (e)=>{
